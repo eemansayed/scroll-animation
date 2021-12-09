7aec366218b173a114403659feea28b519578f48
Eeman Sayed <eeman.sayed.12@gmail.com>
2021-12-09 13:52:52 +0000

    edit function checkboxes

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const boxes = document.querySelectorAll(".box");
 
 window.addEventListener("scroll", checkboxes);
 
+checkboxes();
+
 function checkboxes() {
   const triggerBottom = (window.innerHeight / 5) * 4;
 
